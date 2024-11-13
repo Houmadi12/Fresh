639ec2da6d5abd4635b4f8bbc7c2ce40ee47cdd9
ndeye ngone gningue <gningngone50@gmail.com>
2024-11-13 22:11:26 +0000

    ajouter

--- src/composants/sectionProducteurs/Confiance.jsx
+++ src/composants/sectionProducteurs/Confiance.jsx
@@ -6,21 +6,21 @@ import logo4 from '../../assets/téléchargement.png';
 
 function Confiance() {
     return (
-        <div className='bg-[#f9fafb] mt-9 mb-8'>
+        <div className='bg-[#f9fafb] mt-9 mb-8 pb-7'>
             <div className='container mx-auto '>
                 <h1 className='text-3xl text-center mb-5 mt-7 p-5'>Ils nous font confiance</h1>
-                <div className='flex justify-center items-center  px-5 p-5 mb-8'>
+                <div className='flex justify-center items-center  px-5 p-5 mb-10'>
                     <div>
-                        <img src={logo1} alt="" className='w-2/4'/>
+                        <img src={logo1} alt="" className='w-3/4'/>
                     </div>
                     <div>
                         <img src={logo2} alt="" className='w-3/4'/>
                     </div>
                     <div>
-                        <img src={logo3} alt="" className='w-2/4'/>
+                        <img src={logo3} alt="" className='w-3/4'/>
                     </div>
                     <div>
-                        <img src={logo4} alt="" className='w-2/4'/>
+                        <img src={logo4} alt="" className='w-3/4'/>
                     </div>
                 </div>
             </div>
--- src/composants/sectionProducteurs/SectionNous.jsx
+++ src/composants/sectionProducteurs/SectionNous.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
 
-function SectionNous() {
+import React from 'react';
+import Slider from 'react-slick';
+import profile from '../../assets/profils.c1dd4ac82d5d090db021.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+const SectionNous = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        focusOnSelect: true,
+    };
     return (
-        <div>
-            <div>
-                <h1 classNameName='text-3xl'>Ils parlent de nous </h1>
-                <div>
-                </div>
+        <div className='container mx-auto w-3/4 mb-8'>
+            <h1 className='text-3xl text-center mb-5 mt-7'>Ils parlent de nous</h1>
+            <div className="carrousel-container rounded-lg shadow-lg ">
+                <Slider {...settings}>
+                    <div className="slide text-sm bg-[#dcfce7] text-center " >
+                        <img src={profile} alt="" className='rounded-full mb-4 max-w-xs mx-auto p-3 '/>
+                        <h3>Jean Dupont</h3>
+                        <p>B2B solutions</p>
+                        <p>Un grand merci à cette plateforme qui nous a permis de trouver des producteurs locaux de qualité. Les produits sont excellents et nos employés sont ravis.</p>
+                    </div>
+                    <div className="slide text-sm bg-[#000000] text-center text-white" >
+                        <img src={profile} alt="" className='rounded-full mb-4 max-w-xs mx-auto p-3'/>
+                        <p>Angele Martinez</p>
+                        <p>B2B Solutions</p>
+                        <p>En tant qu'agricultrice locale, cette plateforme m'a permis d'atteindre de nouveaux marchés et de faire connaître mes produits. Une expérience unique!</p>
+                    </div>
+                    <div className="slide text-sm bg-[#dcfce7] text-center" >
+                        <img src={profile} alt="" className='rounded-full mb-4 max-w-xs mx-auto p-3'/>
+                        <p>Javier Escobar</p>
+                        <p>B2B Solutions</p>
+                        <p>Être en contact avec des clients directement et fournir des produits frais est un vrai plaisir. Merci pour cette opportunité Merci pour cette opportunité!</p>
+                    </div>
+                </Slider>
             </div>
         </div>
-    )
+    );
 }
+
 export default SectionNous;
