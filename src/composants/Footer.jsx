acd65b5d2b23bb169f2a1ddc7562ea1dfd534ae1
Ambdouroihmane Soilihi <ambdousoilihiit@gmail.com>
2024-11-14 17:47:06 +0000

    mise à jour des changement

--- src/composants/Footer.jsx
+++ src/composants/Footer.jsx
@@ -1,5 +1,3 @@
-import Logo from '../assets/logo.png'
-import { useState } from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { CiTwitter } from "react-icons/ci";
@@ -9,53 +7,53 @@ export default function Footer() {
    
     return (
         <div>
-            <footer class="bg-black text-white py-8">
-                <div class="container mx-auto px-4">
-                    <div class="flex flex-col md:flex-row justify-between items-start md:items-center">
-                        <div class="mb-6 md:mb-0">
-                            <h1 class="  font-bold mb-4 text-lg">Assistance</h1>
+            <footer className="bg-black text-white py-8">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+                        <div className="mb-6 md:mb-0">
+                            <h1 className="  font-bold mb-4 text-lg">Assistance</h1>
                             <ul>
-                                <li><a href="#" class="text-gray-400 hover:text-white">FAQ</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Contactez-nous</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Support technique</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">FAQ</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Contactez-nous</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Support technique</a></li>
                             </ul>
                         </div>
-                        <div class="mb-6 md:mb-0">
-                            <h1 class=" font-bold mb-4 text-lg">À propos de</h1>
+                        <div className="mb-6 md:mb-0">
+                            <h1 className=" font-bold mb-4 text-lg">À propos de</h1>
                             <ul>
-                                <li><a href="#" class="text-gray-400 hover:text-white">À propos de nous</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Politique de confidentialité</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Politique en matière de cookies</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Règlement général sur la protection des données</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">À propos de nous</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Politique de confidentialité</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Politique en matière de cookies</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Règlement général sur la protection des données</a></li>
                             </ul>
                         </div>
-                        <div class="mb-6 md:mb-0">
-                            <h1 class=" font-bold mb-4 text-lg">Conditions</h1>
+                        <div className="mb-6 md:mb-0">
+                            <h1 className=" font-bold mb-4 text-lg">Conditions</h1>
                             <ul>
                                 <li>
-                                    <a href="#" class="text-gray-400 hover:text-white">Conditions générales</a></li>
-                                <li><a href="#" class="text-gray-400 hover:text-white">Conditions de facturation</a></li>
+                                    <a href="#" className="text-gray-400 hover:text-white">Conditions générales</a></li>
+                                <li><a href="#" className="text-gray-400 hover:text-white">Conditions de facturation</a></li>
                             </ul>
                         </div>
-                        <div class="mb-6 md:mb-0 flex space-x-4">
-                            <a href="#" class="text-gray-400 ">
+                        <div className="mb-6 md:mb-0 flex space-x-4">
+                            <a href="#" className="text-gray-400 ">
                                 <FaFacebook class="w-6 h-6 text-white"/>
                             </a>
-                            <a href="#" class="text-gray-400">
+                            <a href="#" className="text-gray-400">
                                
-                                <FaInstagram class="w-6 h-6 text-white" />
+                                <FaInstagram className="w-6 h-6 text-white" />
                             </a>
-                            <a href="#" class="text-gray-400 ">
+                            <a href="#" className="text-gray-400 ">
                             
-                                <CiTwitter class="w-6 h-6 text-white font-bold" />
+                                <CiTwitter className="w-6 h-6 text-white font-bold" />
                             </a>
-                            <a href="#" class="text-gray-400 ">
+                            <a href="#" className="text-gray-400 ">
                                
-                                <FaLinkedin class="w-6 h-6 text-white" />
+                                <FaLinkedin className="w-6 h-6 text-white" />
                             </a>
                         </div>
                     </div>
-                    <div class="text-center text-gray-400 mt-8">© 2024 FRESH. Tous droits réservés.</div>
+                    <div className="text-center text-gray-400 mt-8">© 2024 FRESH. Tous droits réservés.</div>
                 </div>
             </footer>
         </div>
